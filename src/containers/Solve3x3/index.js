9757ca49e99c5ad53e7a0b86b2fec631a337320a
Ariel Bobadilla <arielbdilla@gmail.com>
2018-01-02 21:58:21 -0500

    Added subheading on 3x3 page

--- src/containers/Solve3x3/index.js
+++ src/containers/Solve3x3/index.js
@@ -4,9 +4,8 @@ class Solve3x3 extends React.Component {
   render() {
     return (
       <div className='container bleed-top'>
-        <h1 className='title-big'>
-          3x3
-        </h1>
+        <h1 className='title-big'>3x3</h1>
+        <h2 className='article-name'>How to Solve</h2>
       </div>
     );
   }
