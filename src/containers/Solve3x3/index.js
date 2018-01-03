c6e0b7560c6cfaa0cc46d8e5817b3a9f9de5de8e
Ariel Bobadilla <arielbdilla@gmail.com>
2018-01-02 22:37:06 -0500

    added card and styles for first step of 3x3

--- src/containers/Solve3x3/index.js
+++ src/containers/Solve3x3/index.js
@@ -6,6 +6,30 @@ class Solve3x3 extends React.Component {
       <div className='container bleed-top'>
         <h1 className='title-big'>3x3</h1>
         <h2 className='article-name'>How to Solve</h2>
+        <section className='instructions instructions-3x3'>
+          <section className='step1'>
+            <section className='step-card'>
+              <h3>Notation</h3>
+              <p>Solving a Rubik’s cube involves using algorithms, which are sets of moves to accomplish a task. Rubik’s cube algorithms use the following notation:</p>
+              <ul>
+                <li>U: Rotate the top layer clockwise</li>
+                <li>D: Rotate the bottom layer clockwise</li>
+                <li>L: Rotate the left layer clockwise</li>
+                <li>R: Rotate the right layer clockwise</li>
+                <li>F: Rotate the front layer clockwise</li>
+                <li>B: Rotate the back layer clockwise</li>
+              </ul>
+              <p>When a move has an apostrophe (') after it, then that move should be done counter-clockwise.</p>
+              <p>When a move has a two (2) after it, then that move should be performed twice.</p>
+              <p className='text-warning'>In this tutorial, the front face will always be shown as the green face in diagrams.</p>
+              <p>Example algorithms:</p>
+              <ul>
+                <li>R U R' U R U2 R'</li>
+                <li>F R U' R' U' R U R' F' R U R' U' R' F R F'</li>
+              </ul>
+            </section>
+          </section>
+        </section>
       </div>
     );
   }
